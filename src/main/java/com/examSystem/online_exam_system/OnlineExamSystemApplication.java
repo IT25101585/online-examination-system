cfeb4061ef753b3115135453b61a5d4b84b65cc9
it25101585 <it25101585@my.sliit.lk>
2026-05-05 20:37:18 +0530

    add user management classes for login/register authorization

--- src/main/java/com/examSystem/online_exam_system/OnlineExamSystemApplication.java
+++ src/main/java/com/examSystem/online_exam_system/OnlineExamSystemApplication.java
@@ -1,7 +1,11 @@
 package com.examSystem.online_exam_system;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
 
 @SpringBootApplication
 public class OnlineExamSystemApplication {
@@ -10,4 +14,22 @@ public class OnlineExamSystemApplication {
 		SpringApplication.run(OnlineExamSystemApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner openBrowser() {
+		return args -> {
+			try {
+				String url = "http://localhost:8080/users/login";
+
+				// This works more reliably than Desktop
+				if (java.awt.Desktop.isDesktopSupported()) {
+					new ProcessBuilder(
+							"cmd", "/c", "start", url
+					).start();
+				}
+
+			} catch (Exception e) {
+				System.out.println("Auto browser open failed: " + e.getMessage());
+			}
+		};
+	}
 }
