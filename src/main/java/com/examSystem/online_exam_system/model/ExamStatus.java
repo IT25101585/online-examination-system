82889fb2f282e3584f0fe1171a9deda3917d326c
IT25103715 <it25103715@my.sliit.lk>
2026-05-19 07:52:02 +0530

    new update

--- src/main/java/com/examSystem/online_exam_system/model/ExamStatus.java
+++ src/main/java/com/examSystem/online_exam_system/model/ExamStatus.java
@@ -1,9 +1,9 @@
 package com.examSystem.online_exam_system.model;
 
 public enum ExamStatus {
-    DRAFT, // exam is still being prepared, students can't see it
-    PENDING,  //teacher submitted for admin approval
+    DRAFT,      // exam is still being prepared, students can't see it
+    PENDING,    // teacher submitted for admin approval
     PUBLISHED,  // exam is ready, students can see and take it
-    REJECTED //admin rejected- needs revision
+    REJECTED,   // admin rejected- needs revision
+    UNPUBLISHED // admin force unpublished the exam- needs teacher review
 }
-
